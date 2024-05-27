9282b9810081a31329baca7796b5d9753e1928b8
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2024-05-27 13:37:58 -0700

    adding v5.0.5

--- glow/src/config.h
+++ glow/src/config.h
@@ -3,10 +3,10 @@
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
 #define ORYX_CONFIGURATOR
-#undef RGB_MATRIX_VAL_STEP
-#define RGB_MATRIX_VAL_STEP 17
-
 #define USB_SUSPEND_WAKEUP_DELAY 0
+#define FIRMWARE_VERSION u8"jvP9x/VDpMM"
+#define RAW_USAGE_PAGE 0xFF60
+#define RAW_USAGE_ID 0x61
 #define LAYER_STATE_8BIT
 
 #define RGB_MATRIX_STARTUP_SPD 60
